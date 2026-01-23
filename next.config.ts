0737467fc114cfa486b6de8d71ed26bfac538dbf
VikasThakur <vikasthakur1723@gmail.com>
2026-01-23 22:53:51 +0530

    Add homepage layout with coin overview and DateTable component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.coingecko.com',
+        port: '',
+        pathname: '/coins/images/**',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
