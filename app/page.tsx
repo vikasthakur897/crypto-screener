56e16a4800855a160d91bbc6893a1dfe0361bb44
VikasThakur <vikasthakur1723@gmail.com>
2026-02-01 15:45:59 +0530

    Add Categories component to display top categories with data table

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import Categories from "@/components/home/Categories";
 import CoinOverview from "@/components/home/CoinOverview";
-import { CoinOverviewFallback, TrendingCoinsFallback } from "@/components/home/fallback";
+import { CategoriesFallback, CoinOverviewFallback, TrendingCoinsFallback } from "@/components/home/fallback";
 import TrendingCoins from "@/components/home/TrendingCoins";
 import { Suspense } from "react";
 
@@ -17,7 +18,9 @@ const page = async () => {
       </section>
 
       <section className="w-full mt-7 space-y-4">
-        <p>Category</p>
+        <Suspense fallback={<CategoriesFallback />}>
+        <Categories />
+        </Suspense>
       </section>
     </main>
   );
